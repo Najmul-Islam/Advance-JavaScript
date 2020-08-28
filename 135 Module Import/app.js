7adfd6e2d9d8f2ed55ab9fe6e725b48a13bcd63d
Najmul-Islam <najmulislam519@gmail.com>
2020-08-28 20:07:06 +0600

    complete this lesson

---
+++ 135 Module Import/app.js
@@ -0,0 +1,7 @@
+// Default Import Class
+
+import Nokia, { show, a } from "./mobile.js";
+const n = new Nokia();
+n.volumnUp();
+show();
+console.log(a);
---
+++ 135 Module Import/mobile.js
@@ -0,0 +1,14 @@
+// Default Export Class
+
+class Nokia {
+    volumnUp() {
+        console.log("High Volumn");
+    }
+}
+
+function show() {
+    console.log("Hello Module");
+}
+export const a = 10;
+export default Nokia;
+export { show }
